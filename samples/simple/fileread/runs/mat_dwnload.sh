a04f7b4cf00c71c2f74eb754d446bd000192f299
Salvatore Filippone <salvatore.filippone@uniroma2.it>
2021-07-15 12:16:56 +0200

    Simple shell to download sample matrices

---
+++ samples/simple/fileread/runs/mat_dwnload.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+wget https://suitesparse-collection-website.herokuapp.com/MM/Schmid/thermal2.tar.gz
+wget https://suitesparse-collection-website.herokuapp.com/MM/Bourchtein/atmosmodd.tar.gz
