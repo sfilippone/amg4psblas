e46f22a37c826a3e459be387315a3d48256e4e1f
Salvatore Filippone <salvatore.filippone@uniroma2.it>
2022-05-24 13:12:59 +0200

    Update for new SPALL/SPASB interface.

--- samples/advanced/pdegen/amg_s_genpde_mod.F90
+++ samples/advanced/pdegen/amg_s_genpde_mod.F90
@@ -500,9 +500,9 @@ contains
     t1 = psb_wtime()
     if (info == psb_success_) then
       if (present(amold)) then
-        call psb_spasb(a,desc_a,info,dupl=psb_dupl_err_,mold=amold)
+        call psb_spasb(a,desc_a,info,mold=amold)
       else
-        call psb_spasb(a,desc_a,info,dupl=psb_dupl_err_,afmt=afmt)
+        call psb_spasb(a,desc_a,info,afmt=afmt)
       end if
     end if
     call psb_barrier(ctxt)
@@ -922,9 +922,9 @@ contains
     t1 = psb_wtime()
     if (info == psb_success_) then
       if (present(amold)) then
-        call psb_spasb(a,desc_a,info,dupl=psb_dupl_err_,mold=amold)
+        call psb_spasb(a,desc_a,info,mold=amold)
       else
-        call psb_spasb(a,desc_a,info,dupl=psb_dupl_err_,afmt=afmt)
+        call psb_spasb(a,desc_a,info,afmt=afmt)
       end if
     end if
     call psb_barrier(ctxt)
