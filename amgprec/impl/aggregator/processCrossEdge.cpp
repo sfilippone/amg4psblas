ccac816f52b4be91a0231c14c3a4983c85285e04
StefanoPetrilli <stefanop_1999@hotmail.it>
2022-07-12 13:24:12 -0500

    processCrossEdge small refactoring

--- amgprec/impl/aggregator/processCrossEdge.cpp
+++ amgprec/impl/aggregator/processCrossEdge.cpp
@@ -1,11 +1,10 @@
 #include "MatchBoxPC.h"
 
 void PROCESS_CROSS_EDGE(vector<MilanLongInt> &Counter,
-                               map<MilanLongInt, MilanLongInt> &Ghost2LocalMap,
-                               MilanLongInt edge,
-                               MilanLongInt *SPtr)
+                        map<MilanLongInt, MilanLongInt> &Ghost2LocalMap,
+                        MilanLongInt edge,
+                        MilanLongInt *SPtr)
 {
-    MilanLongInt S = *SPtr;
     // Decrement the counter:
     // Start: PARALLEL_PROCESS_CROSS_EDGE_B
     if (Counter[Ghost2LocalMap[edge]] > 0)
@@ -13,13 +12,13 @@ void PROCESS_CROSS_EDGE(vector<MilanLongInt> &Counter,
         Counter[Ghost2LocalMap[edge]] -= 1; // Decrement
         if (Counter[Ghost2LocalMap[edge]] == 0)
         {
-            S--; // Decrement S
+            (*SPtr)--; // Decrement S
 #ifdef PRINT_DEBUG_INFO_
             cout << "\n(" << myRank << ")Decrementing S: Ghost vertex " << edge << " has received all its messages";
             fflush(stdout);
 #endif
         }
+
     } // End of if Counter[edge] > 0
       // End: PARALLEL_PROCESS_CROSS_EDGE_B
-    *SPtr = S;
 }
