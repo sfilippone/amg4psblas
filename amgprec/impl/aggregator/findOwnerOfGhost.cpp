44f174a5714063b50cb08a689b237258aea2f75b
StefanoPetrilli <stefanop_1999@hotmail.it>
2022-07-17 13:44:58 -0500

    findOwnerOfGhost optimization and refactor

--- amgprec/impl/aggregator/findOwnerOfGhost.cpp
+++ amgprec/impl/aggregator/findOwnerOfGhost.cpp
@@ -1,48 +1,29 @@
 #include "MatchBoxPC.h"
 
-
-//TODO parallelize this
-///Find the owner of a ghost node:
+/// Find the owner of a ghost node:
 MilanInt findOwnerOfGhost(MilanLongInt vtxIndex, MilanLongInt *mVerDistance,
-                                     MilanInt myRank, MilanInt numProcs) {
+                          MilanInt myRank, MilanInt numProcs)
+{
 
   MilanLongInt mStartInd = mVerDistance[myRank];
   MilanInt Start = 0;
   MilanInt End = numProcs;
   MilanInt Current = 0;
 
-#if 0
-  if ( vtxIndex < mStartInd )
-    End = myRank;
-  else
-    Start = myRank;
-#endif
+  while (Start <= End)
+  {
+    Current = (End + Start) / 2;
+    // CASE-1:
+    if (mVerDistance[Current] == vtxIndex) return Current;
+    else // CASE 2:
+      if (mVerDistance[Current] > vtxIndex)
+        End = Current - 1;
+      else // CASE 3:
+        Start = Current + 1;
+  } // End of While()
 
-  while ( Start <= End ) {
-    Current = (End + Start)/2;
-    //CASE-1:
-    if ( mVerDistance[Current] == vtxIndex ) {
-      while ( mVerDistance[Current+1] == vtxIndex ) {
-	Current++;
-	if ( Current == numProcs )
-	  return (-1);
-      }
-      return (Current);
-    }
-    else { //CASE 2:
-      if ( mVerDistance[Current] > vtxIndex )
-	End = Current - 1;
-      else //CASE 3:
-	Start = Current + 1;
-    }
-  } //End of While()
-  if ( Current == 0 )
-    return (Current);
-  else {
-    if ( mVerDistance[Current] > vtxIndex )
-      return (Current-1);
-    else
-      return (Current);
-  } //End of else
-  return (-1); //It should not reach here!
-} //End of findOwnerOfGhost()
+ if (mVerDistance[Current] > vtxIndex)
+      return (Current - 1);
+
+  return Current;
+} // End of findOwnerOfGhost()
