f3123f1acc8c6bc706639cdea2555dc09f3af779
sfilippone <filippone.salvatore@gmail.com>
2025-03-24 17:23:42 +0100

    Improve output of memory occupation

--- samples/advanced/pdegen/amg_d_pde3d.F90
+++ samples/advanced/pdegen/amg_d_pde3d.F90
@@ -103,7 +103,7 @@ program amg_d_pde3d
 
   ! solver parameters
   integer(psb_ipk_)        :: iter, itmax,itrace, istopc, irst, nlv
-  integer(psb_epk_) :: amatsize, precsize, descsize
+  integer(psb_epk_) :: amatsize, precsize, descsize, vecsize
   real(psb_dpk_)   :: err, resmx, resmxp
 
   ! Solver data
@@ -541,10 +541,12 @@ program amg_d_pde3d
   resmx  = psb_genrm2(r,desc_a,info)
   resmxp = psb_geamax(r,desc_a,info)
 
+  vecsize = x%sizeof()
   amatsize = a%sizeof()
   descsize = desc_a%sizeof()
   precsize = prec%sizeof()
   system_size = desc_a%get_global_rows()
+  call psb_sum(ctxt,vecsize)
   call psb_sum(ctxt,amatsize)
   call psb_sum(ctxt,descsize)
   call psb_sum(ctxt,precsize)
@@ -568,9 +570,12 @@ program amg_d_pde3d
     write(psb_out_unit,'("Total time                         : ",es12.5)') tslv+tprec+thier
     write(psb_out_unit,'("Residual 2-norm                    : ",es12.5)') resmx
     write(psb_out_unit,'("Residual inf-norm                  : ",es12.5)') resmxp
+    write(psb_out_unit,'("Total memory occupation for X      : ",i12)') vecsize
     write(psb_out_unit,'("Total memory occupation for A      : ",i12)') amatsize
     write(psb_out_unit,'("Total memory occupation for DESC_A : ",i12)') descsize
     write(psb_out_unit,'("Total memory occupation for PREC   : ",i12)') precsize
+    write(psb_out_unit,'("Total memory occupation            : ",i12)') &
+         & amatsize + descsize+precsize+2*vecsize    
     write(psb_out_unit,'("Storage format for A               : ",a  )') a%get_fmt()
     write(psb_out_unit,'("Storage format for DESC_A          : ",a  )') desc_a%get_fmt()
 
