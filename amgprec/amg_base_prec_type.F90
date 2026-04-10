0e1d7de857d0ed78f0c73e48349ab36c34615244
sfilippone <filippone.salvatore@gmail.com>
2026-04-10 13:37:51 +0200

    Update VERSION

--- amgprec/amg_base_prec_type.F90
+++ amgprec/amg_base_prec_type.F90
@@ -84,7 +84,7 @@ module amg_base_prec_type
   character(len=*), parameter   :: amg_version_string_ = "1.2.0"
   integer(psb_ipk_), parameter  :: amg_version_major_  = 1
   integer(psb_ipk_), parameter  :: amg_version_minor_  = 2
-  integer(psb_ipk_), parameter  :: amg_patchlevel_     = 0
+  integer(psb_ipk_), parameter  :: amg_patchlevel_     = 1
 
   type amg_ml_parms
     integer(psb_ipk_) :: sweeps_pre, sweeps_post
