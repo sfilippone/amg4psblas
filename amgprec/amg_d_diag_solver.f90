7d38be4158f0c03ab9a97c0124c109fcf3ccb00f
IvanDB <ivan.debiasi@phd.unipi.it>
2026-05-02 14:39:31 +0000

    Implementation of "apply" multivectors methods for the diag solver type

--- amgprec/amg_d_diag_solver.f90
+++ amgprec/amg_d_diag_solver.f90
@@ -47,73 +47,138 @@
 !    what are commonly known as the classic Jacobi iterations
 !
 module amg_d_diag_solver
-
   use amg_d_base_solver_mod
 
   type, extends(amg_d_base_solver_type) :: amg_d_diag_solver_type
     type(psb_d_vect_type), allocatable :: dv
     real(psb_dpk_), allocatable        :: d(:)
   contains
-    procedure, pass(sv) :: dump    => amg_d_diag_solver_dmp
-    procedure, pass(sv) :: build   => amg_d_diag_solver_bld
-    procedure, pass(sv) :: cnv     => amg_d_diag_solver_cnv
-    procedure, pass(sv) :: clone   => amg_d_diag_solver_clone
-    procedure, pass(sv) :: clear_data  => amg_d_diag_solver_clear_data
-    procedure, pass(sv) :: apply_v => amg_d_diag_solver_apply_vect
-    procedure, pass(sv) :: apply_a => amg_d_diag_solver_apply
-    procedure, pass(sv) :: free    => d_diag_solver_free
-    procedure, pass(sv) :: descr   => d_diag_solver_descr
-    procedure, pass(sv) :: sizeof  => d_diag_solver_sizeof
-    procedure, pass(sv) :: get_nzeros  => d_diag_solver_get_nzeros
-    procedure, nopass   :: get_fmt   => d_diag_solver_get_fmt
-    procedure, nopass   :: get_id    => d_diag_solver_get_id
-  end type amg_d_diag_solver_type
+    procedure, pass(sv) :: apply_a      => amg_d_diag_solver_apply
+    procedure, pass(sv) :: apply_v      => amg_d_diag_solver_apply_vect
+    ! procedure, pass(sv) :: apply_mv     => amg_d_base_solver_apply_mvect
+    procedure, pass(sv) :: apply_v_mv   => amg_d_diag_solver_apply_vect_mvect
+    procedure, pass(sv) :: apply_mv_v   => amg_d_diag_solver_apply_mvect_vect
+    procedure, pass(sv) :: apply_mv_col => amg_d_diag_solver_apply_mvect_col
 
+    procedure, pass(sv) :: dump   => amg_d_diag_solver_dmp
+    procedure, pass(sv) :: clone  => amg_d_diag_solver_clone
+    procedure, pass(sv) :: clear_data  => amg_d_diag_solver_clear_data
+    procedure, pass(sv) :: build  => amg_d_diag_solver_bld
+    procedure, pass(sv) :: cnv    => amg_d_diag_solver_cnv
+    procedure, pass(sv) :: free   => d_diag_solver_free
+    procedure, pass(sv) :: descr  => d_diag_solver_descr
+    procedure, pass(sv) :: sizeof => d_diag_solver_sizeof
+    procedure, pass(sv) :: get_nzeros => d_diag_solver_get_nzeros
+    procedure, nopass   :: get_fmt    => d_diag_solver_get_fmt
+    procedure, nopass   :: get_id     => d_diag_solver_get_id
+  end type amg_d_diag_solver_type
 
   private :: d_diag_solver_free,  d_diag_solver_descr, &
        & d_diag_solver_sizeof, d_diag_solver_get_nzeros, &
        & d_diag_solver_get_fmt, d_diag_solver_get_id
 
-
   interface 
-    subroutine amg_d_diag_solver_apply_vect(alpha,sv,x,beta,y,desc_data,& 
-         & trans,work,wv,info,init,initu)
-      import :: psb_desc_type, psb_dspmat_type,  psb_d_base_sparse_mat, &
-       & psb_d_vect_type, psb_d_base_vect_type, psb_dpk_, &
-       & amg_d_diag_solver_type, psb_ipk_
-      type(psb_desc_type), intent(in)                :: desc_data
-      class(amg_d_diag_solver_type), intent(inout) :: sv
-      type(psb_d_vect_type), intent(inout)         :: x
-      type(psb_d_vect_type), intent(inout)         :: y
-      real(psb_dpk_),intent(in)                     :: alpha,beta
-      character(len=1),intent(in)                    :: trans
-      real(psb_dpk_),target, intent(inout)          :: work(:)
-      type(psb_d_vect_type),intent(inout)          :: wv(:)
-      integer(psb_ipk_), intent(out)                 :: info
-      character, intent(in), optional                :: init
-      type(psb_d_vect_type),intent(inout), optional   :: initu
+    subroutine amg_d_diag_solver_apply(alpha, sv, x, beta, y, &
+         & desc_data, trans, work, info, init, initu)
+      import :: psb_dpk_, amg_d_diag_solver_type, &
+              & psb_desc_type, psb_ipk_
+      implicit none
+      real(psb_dpk_),intent(in)                     :: alpha, beta
+      class(amg_d_diag_solver_type), intent(inout)  :: sv
+      real(psb_dpk_), intent(inout)                 :: x(:), y(:)
+      type(psb_desc_type), intent(in)               :: desc_data
+      character(len=1), intent(in)                  :: trans
+      real(psb_dpk_), target, intent(inout)         :: work(:)
+      integer(psb_ipk_), intent(out)                :: info
+      character, intent(in), optional         :: init
+      real(psb_dpk_), intent(inout), optional :: initu(:)
+    end subroutine amg_d_diag_solver_apply
+  end interface
+  
+  interface 
+    subroutine amg_d_diag_solver_apply_vect(alpha, sv, x, beta, y, & 
+         & desc_data, trans, work, wv, info, init, initu)
+      import :: psb_dpk_, amg_d_diag_solver_type, &
+              & psb_d_vect_type, psb_desc_type, psb_ipk_
+      implicit none
+      real(psb_dpk_),intent(in)                     :: alpha, beta
+      class(amg_d_diag_solver_type), intent(inout)  :: sv
+      type(psb_d_vect_type), intent(inout)          :: x, y
+      type(psb_desc_type), intent(in)               :: desc_data
+      character(len=1),intent(in)                   :: trans
+      real(psb_dpk_), target, intent(inout)         :: work(:)
+      type(psb_d_vect_type), intent(inout)          :: wv(:)
+      integer(psb_ipk_), intent(out)                :: info
+      character, intent(in), optional                 :: init
+      type(psb_d_vect_type), intent(inout), optional  :: initu
     end subroutine amg_d_diag_solver_apply_vect
   end interface
   
   interface 
-    subroutine amg_d_diag_solver_apply(alpha,sv,x,beta,y,desc_data,&
-         & trans,work,info,init,initu)
-      import :: psb_desc_type, psb_dspmat_type,  psb_d_base_sparse_mat, &
-       & psb_d_vect_type, psb_d_base_vect_type, psb_dpk_, &
-       & amg_d_diag_solver_type, psb_ipk_
-      type(psb_desc_type), intent(in)            :: desc_data
-      class(amg_d_diag_solver_type), intent(inout) :: sv
-      real(psb_dpk_), intent(inout)             :: x(:)
-      real(psb_dpk_), intent(inout)             :: y(:)
-      real(psb_dpk_),intent(in)                 :: alpha,beta
-      character(len=1),intent(in)                :: trans
-      real(psb_dpk_),target, intent(inout)      :: work(:)
-      integer(psb_ipk_), intent(out)             :: info
-      character, intent(in), optional       :: init
-      real(psb_dpk_),intent(inout), optional :: initu(:)
-    end subroutine amg_d_diag_solver_apply
+    subroutine amg_d_diag_solver_apply_vect_mvect(alpha, sv, x, beta, y, idx_y, & 
+         & desc_data, trans, work, wv, info, init, initu)
+      import :: psb_dpk_, amg_d_diag_solver_type, &
+              & psb_d_multivect_type, psb_ipk_, &
+              & psb_desc_type, psb_d_vect_type
+      implicit none
+      real(psb_dpk_),intent(in)                     :: alpha, beta
+      class(amg_d_diag_solver_type), intent(inout)  :: sv
+      type(psb_d_vect_type), intent(inout)          :: x
+      type(psb_d_multivect_type), intent(inout)     :: y
+      integer(psb_ipk_), intent(in)                 :: idx_y
+      type(psb_desc_type), intent(in)               :: desc_data
+      character(len=1),intent(in)                   :: trans
+      real(psb_dpk_), target, intent(inout)         :: work(:)
+      type(psb_d_vect_type), intent(inout)          :: wv(:)
+      integer(psb_ipk_), intent(out)                :: info
+      character, intent(in), optional                 :: init
+      type(psb_d_vect_type), intent(inout), optional  :: initu
+    end subroutine amg_d_diag_solver_apply_vect_mvect
+  end interface
+
+  interface 
+    subroutine amg_d_diag_solver_apply_mvect_vect(alpha, sv, x, idx_x, beta, y, & 
+         & desc_data, trans, work, wv, info, init, initu)
+      import :: psb_dpk_, amg_d_diag_solver_type, &
+              & psb_d_multivect_type, psb_ipk_, &
+              & psb_desc_type, psb_d_vect_type
+      implicit none
+      real(psb_dpk_),intent(in)                     :: alpha, beta
+      class(amg_d_diag_solver_type), intent(inout)  :: sv
+      type(psb_d_multivect_type), intent(inout)     :: x
+      integer(psb_ipk_), intent(in)                 :: idx_x
+      type(psb_d_vect_type), intent(inout)          :: y
+      type(psb_desc_type), intent(in)               :: desc_data
+      character(len=1),intent(in)                   :: trans
+      real(psb_dpk_), target, intent(inout)         :: work(:)
+      type(psb_d_vect_type), intent(inout)          :: wv(:)
+      integer(psb_ipk_), intent(out)                :: info
+      character, intent(in), optional                 :: init
+      type(psb_d_vect_type), intent(inout), optional  :: initu
+    end subroutine amg_d_diag_solver_apply_mvect_vect
   end interface
   
+  interface 
+    subroutine amg_d_diag_solver_apply_mvect_col(alpha, sv, x, idx_x, beta, y, idx_y, &
+                  & desc_data, trans, work, wv, info, init, initu)
+      import :: psb_dpk_, amg_d_diag_solver_type, &
+              & psb_d_multivect_type, psb_ipk_, &
+              & psb_desc_type, psb_d_vect_type
+      implicit none 
+      real(psb_dpk_), intent(in)                    :: alpha, beta
+      class(amg_d_diag_solver_type), intent(inout)  :: sv
+      type(psb_d_multivect_type), intent(inout)     :: x, y
+      integer(psb_ipk_), intent(in)                 :: idx_x, idx_y
+      type(psb_desc_type), intent(in)               :: desc_data
+      character(len=1), intent(in)                  :: trans
+      real(psb_dpk_), target, intent(inout)         :: work(:)
+      type(psb_d_vect_type), intent(inout)          :: wv(:)
+      integer(psb_ipk_), intent(out)                :: info
+      character, intent(in), optional                 :: init
+      type(psb_d_vect_type), intent(inout), optional  :: initu
+    end subroutine amg_d_diag_solver_apply_mvect_col
+  end interface
+
   interface 
     subroutine amg_d_diag_solver_bld(a,desc_a,sv,info,b,amold,vmold,imold)
       import :: psb_desc_type, psb_dspmat_type,  psb_d_base_sparse_mat, &
